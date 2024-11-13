a8378892e93787d7fad742f437da999aab32cef1
Becky Williams <beckz14@gmail.com>
2024-11-13 11:50:52 +0000

    trusted origins added

--- my_project/settings.py
+++ my_project/settings.py
@@ -82,6 +82,11 @@ DATABASES = {
     }
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ied.net/",
+    "https://*.herokuapp.com"
+]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
